7403fe98628ee8a07ec6f3a3be7888a7427d4484
sewmisithara <sewmisithara23520@gmail.com>
2023-05-10 10:01:50 +0530

    end new portfolio

---
+++ assest/js/slick.js
@@ -0,0 +1,39 @@
+
+window.addEventListener('load',() => { 
+    $(".swiper").slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        pauseOnHover: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+
+ })
