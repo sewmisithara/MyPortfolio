892f994b589d4aa11064c6445ee7f032594b187a
sewmisithara <sewmisithara23520@gmail.com>
2023-06-05 17:01:23 +0530

    Knight and Qua update

---
+++ assest/assignment/JS/SPA/posjs.js
@@ -0,0 +1,12 @@
+//===================== MODULE ===========================
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+
+
+
